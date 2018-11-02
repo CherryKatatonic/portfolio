a4591284c4ef6c9d76c9c3237a84d0baf925a801
Kat Grennan <turret24@gmail.com>
2018-11-01 23:16:56 -0500

    TODO - figure out why smooth scroll scripts don't work on home.html

---
+++ src/main/resources/static/js/smooth-scroll.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function(){
+    $('a').click(function(){
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        return false;
+    });
+});*/
+
+// TODO - figure out why smooth scroll scripts don't work on home.html
